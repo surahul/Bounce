999533d4843f80c3376b1f9cfa3ddf0971301719
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-10-25 13:36:43 +0530

    Code cleanup

--- bounce/src/main/java/com/rahul/bounce/library/BounceTouchListener.java
+++ bounce/src/main/java/com/rahul/bounce/library/BounceTouchListener.java
@@ -1,4 +1,5 @@
 package com.rahul.bounce.library;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
@@ -17,55 +18,39 @@ import android.widget.ScrollView;
 public class BounceTouchListener implements View.OnTouchListener {
 
 
-
-    public interface OnTranslateListener{
-        void onTranslate(float translation);
-    }
-
-    private static final int DEFAULT_ANIMATION_TIME=600;
-
+    private static final int DEFAULT_ANIMATION_TIME = 600;
+    boolean downCalled = false;
+    OnTranslateListener onTranslateListener;
     private View mMainView;
     private View mContent;
-
-
     private long mAnimationTime = DEFAULT_ANIMATION_TIME;
     private float mDownY;
-    private boolean mSwipingDown,mSwipingUp;
-
+    private boolean mSwipingDown, mSwipingUp;
     private float mTranslationY;
-    private android.view.animation.Interpolator mInterpolator =  new DecelerateInterpolator(3f);
-
+    private android.view.animation.Interpolator mInterpolator = new DecelerateInterpolator(3f);
     private boolean swipUpEnabled = true;
     private int mActivePointerId = -99;
     private float mLastTouchX = -99;
     private float mLastTouchY = -99;
     private int mMaxAbsTranslation = -99;
-    boolean downCalled=false;
-    OnTranslateListener onTranslateListener;
-
-
 
     public BounceTouchListener(View mainScrollableView) {
-        this.mMainView=mainScrollableView;
+        this.mMainView = mainScrollableView;
         this.mContent = this.mMainView;
     }
 
     public BounceTouchListener(View mainScrollableView, int contentResId) {
-        this.mMainView=mainScrollableView;
+        this.mMainView = mainScrollableView;
         this.mContent = this.mMainView.findViewById(contentResId);
     }
 
-
-    public void setOnTranslateListener(OnTranslateListener onTranslateListener){
+    public void setOnTranslateListener(OnTranslateListener onTranslateListener) {
         this.onTranslateListener = onTranslateListener;
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-
-
         final int action = MotionEventCompat.getActionMasked(motionEvent);
-
 
         switch (action) {
             case MotionEvent.ACTION_DOWN: {
@@ -79,21 +64,20 @@ public class BounceTouchListener implements View.OnTouchListener {
                 if (mContent.getTranslationY() > 0) {
                     mDownY = mLastTouchY - (int) Math.pow(mContent.getTranslationY(), 10f / 8f);
                     mContent.animate().cancel();
-
-                }else if(mContent.getTranslationY() < 0){
+                } else if (mContent.getTranslationY() < 0) {
                     mDownY = mLastTouchY + (int) Math.pow(-mContent.getTranslationY(), 10f / 8f);
                     mContent.animate().cancel();
-
-                }
-                else
+                } else {
                     mDownY = mLastTouchY;
+                }
                 view.onTouchEvent(motionEvent);
                 downCalled = true;
-                if (mContent.getTranslationY() == 0)
+                if (mContent.getTranslationY() == 0) {
                     return false;
+                }
             }
             case MotionEvent.ACTION_MOVE: {
-                if(mActivePointerId==-99){
+                if (mActivePointerId == -99) {
                     final int pointerIndex = MotionEventCompat.getActionIndex(motionEvent);
                     final float x = MotionEventCompat.getX(motionEvent, pointerIndex);
                     final float y = MotionEventCompat.getY(motionEvent, pointerIndex);
@@ -103,17 +87,13 @@ public class BounceTouchListener implements View.OnTouchListener {
                     if (mContent.getTranslationY() > 0) {
                         mDownY = mLastTouchY - (int) Math.pow(mContent.getTranslationY(), 10f / 8f);
                         mContent.animate().cancel();
-
-                    }else if(mContent.getTranslationY() < 0){
+                    } else if (mContent.getTranslationY() < 0) {
                         mDownY = mLastTouchY + (int) Math.pow(-mContent.getTranslationY(), 10f / 8f);
                         mContent.animate().cancel();
-
-                    }
-                    else
+                    } else {
                         mDownY = mLastTouchY;
+                    }
                     downCalled = true;
-
-
                 }
                 final int pointerIndex =
                         MotionEventCompat.findPointerIndex(motionEvent, mActivePointerId);
@@ -121,8 +101,9 @@ public class BounceTouchListener implements View.OnTouchListener {
                 final float y = MotionEventCompat.getY(motionEvent, pointerIndex);
 
                 if (!hasHitTop() && !hasHitBottom() || !downCalled) {
-                    if (!downCalled)
+                    if (!downCalled) {
                         downCalled = true;
+                    }
                     mDownY = y;
                     view.onTouchEvent(motionEvent);
                     return false;
@@ -148,7 +129,6 @@ public class BounceTouchListener implements View.OnTouchListener {
                     }
                 }
                 if (mSwipingDown || mSwipingUp) {
-
                     mTranslationY = deltaY;
                     if ((deltaY <= 0 && mSwipingDown) || (deltaY >= 0 && mSwipingUp)) {
                         mTranslationY = 0;
@@ -163,27 +143,23 @@ public class BounceTouchListener implements View.OnTouchListener {
                         break;
                     }
                     int translation = (int) ((deltaY / Math.abs(deltaY)) * Math.pow(Math.abs(deltaY), .8f));
-                    if(mMaxAbsTranslation >0){
-                        if(translation<0){
-                            translation = Math.max(-mMaxAbsTranslation,translation);
-                        }else{
-                            translation = Math.min(mMaxAbsTranslation,translation);
+                    if (mMaxAbsTranslation > 0) {
+                        if (translation < 0) {
+                            translation = Math.max(-mMaxAbsTranslation, translation);
+                        } else {
+                            translation = Math.min(mMaxAbsTranslation, translation);
                         }
                     }
                     mContent.setTranslationY(translation);
-                    if(onTranslateListener!=null)
+                    if (onTranslateListener != null)
                         onTranslateListener.onTranslate(mContent.getTranslationY());
-
-
 
                     return true;
                 }
                 break;
             }
 
-
             case MotionEvent.ACTION_UP: {
-
                 mActivePointerId = -99;
                 // cancel
                 mContent.animate()
@@ -196,15 +172,14 @@ public class BounceTouchListener implements View.OnTouchListener {
                                 ((ValueAnimator) animation).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                                     @Override
                                     public void onAnimationUpdate(ValueAnimator animation) {
-                                        if (onTranslateListener != null)
+                                        if (onTranslateListener != null) {
                                             onTranslateListener.onTranslate(mContent.getTranslationY());
+                                        }
                                     }
                                 });
                                 super.onAnimationStart(animation);
                             }
                         });
-
-
 
                 mTranslationY = 0;
                 mDownY = 0;
@@ -213,13 +188,13 @@ public class BounceTouchListener implements View.OnTouchListener {
                 downCalled = false;
                 break;
             }
+
             case MotionEvent.ACTION_CANCEL: {
                 mActivePointerId = -99;
                 break;
             }
 
             case MotionEvent.ACTION_POINTER_UP: {
-
                 final int pointerIndex = MotionEventCompat.getActionIndex(motionEvent);
                 final int pointerId = MotionEventCompat.getPointerId(motionEvent, pointerIndex);
 
@@ -232,51 +207,45 @@ public class BounceTouchListener implements View.OnTouchListener {
                     if (mContent.getTranslationY() > 0) {
                         mDownY = mLastTouchY - (int) Math.pow(mContent.getTranslationY(), 10f / 8f);
                         mContent.animate().cancel();
-
-                    }else if(mContent.getTranslationY() < 0){
+                    } else if (mContent.getTranslationY() < 0) {
                         mDownY = mLastTouchY + (int) Math.pow(-mContent.getTranslationY(), 10f / 8f);
                         mContent.animate().cancel();
-
                     }
                 }
                 break;
             }
-
-
-
         }
         return false;
     }
 
     private boolean hasHitBottom() {
-        if(mMainView instanceof ScrollView){
-            ScrollView scrollView = (ScrollView)mMainView;
-            View view = scrollView.getChildAt(scrollView.getChildCount()-1);
-            int diff = (view.getBottom()-(scrollView.getHeight()+scrollView.getScrollY()));// Calculate the scrolldiff
-            return diff==0;
-        }else if(mMainView instanceof ListView){
-            ListView listView = (ListView)mMainView;
-            if(listView.getAdapter()!=null){
-                if(listView.getAdapter().getCount()>0){
-                    return listView.getLastVisiblePosition() == listView.getAdapter().getCount() -1 &&
+        if (mMainView instanceof ScrollView) {
+            ScrollView scrollView = (ScrollView) mMainView;
+            View view = scrollView.getChildAt(scrollView.getChildCount() - 1);
+            int diff = (view.getBottom() - (scrollView.getHeight() + scrollView.getScrollY()));// Calculate the scrolldiff
+            return diff == 0;
+        } else if (mMainView instanceof ListView) {
+            ListView listView = (ListView) mMainView;
+            if (listView.getAdapter() != null) {
+                if (listView.getAdapter().getCount() > 0) {
+                    return listView.getLastVisiblePosition() == listView.getAdapter().getCount() - 1 &&
                             listView.getChildAt(listView.getChildCount() - 1).getBottom() <= listView.getHeight();
                 }
             }
-        }
-        else if(mMainView instanceof RecyclerView){
-            RecyclerView recyclerView = (RecyclerView)mMainView;
-            if(recyclerView.getAdapter()!=null&&recyclerView.getLayoutManager()!=null){
+        } else if (mMainView instanceof RecyclerView) {
+            RecyclerView recyclerView = (RecyclerView) mMainView;
+            if (recyclerView.getAdapter() != null && recyclerView.getLayoutManager() != null) {
                 RecyclerView.Adapter adapter = recyclerView.getAdapter();
-                if(adapter.getItemCount()>0){
+                if (adapter.getItemCount() > 0) {
                     RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
-                    if(layoutManager instanceof LinearLayoutManager){
-                        LinearLayoutManager linearLayoutManager = (LinearLayoutManager)layoutManager;
-                        return linearLayoutManager.findLastCompletelyVisibleItemPosition() == adapter.getItemCount()-1;
-                    }else if(layoutManager instanceof StaggeredGridLayoutManager){
-                        StaggeredGridLayoutManager staggeredGridLayoutManager = (StaggeredGridLayoutManager)layoutManager;
+                    if (layoutManager instanceof LinearLayoutManager) {
+                        LinearLayoutManager linearLayoutManager = (LinearLayoutManager) layoutManager;
+                        return linearLayoutManager.findLastCompletelyVisibleItemPosition() == adapter.getItemCount() - 1;
+                    } else if (layoutManager instanceof StaggeredGridLayoutManager) {
+                        StaggeredGridLayoutManager staggeredGridLayoutManager = (StaggeredGridLayoutManager) layoutManager;
                         int[] checks = staggeredGridLayoutManager.findLastCompletelyVisibleItemPositions(null);
-                        for(int check : checks){
-                            if(check == adapter.getItemCount()-1)
+                        for (int check : checks) {
+                            if (check == adapter.getItemCount() - 1)
                                 return true;
                         }
                     }
@@ -286,32 +255,32 @@ public class BounceTouchListener implements View.OnTouchListener {
         return false;
     }
 
-    private boolean hasHitTop(){
-        if(mMainView instanceof ScrollView){
-            ScrollView scrollView = (ScrollView)mMainView;
-            return scrollView.getScrollY()==0;
-        }else if(mMainView instanceof ListView){
-            ListView listView = (ListView)mMainView;
-            if(listView.getAdapter()!=null){
-                if(listView.getAdapter().getCount()>0){
+    private boolean hasHitTop() {
+        if (mMainView instanceof ScrollView) {
+            ScrollView scrollView = (ScrollView) mMainView;
+            return scrollView.getScrollY() == 0;
+        } else if (mMainView instanceof ListView) {
+            ListView listView = (ListView) mMainView;
+            if (listView.getAdapter() != null) {
+                if (listView.getAdapter().getCount() > 0) {
                     return listView.getFirstVisiblePosition() == 0 &&
                             listView.getChildAt(0).getTop() >= 0;
                 }
             }
-        } else if(mMainView instanceof RecyclerView){
-            RecyclerView recyclerView = (RecyclerView)mMainView;
-            if(recyclerView.getAdapter()!=null&&recyclerView.getLayoutManager()!=null){
+        } else if (mMainView instanceof RecyclerView) {
+            RecyclerView recyclerView = (RecyclerView) mMainView;
+            if (recyclerView.getAdapter() != null && recyclerView.getLayoutManager() != null) {
                 RecyclerView.Adapter adapter = recyclerView.getAdapter();
-                if(adapter.getItemCount()>0){
+                if (adapter.getItemCount() > 0) {
                     RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
-                    if(layoutManager instanceof LinearLayoutManager){
-                        LinearLayoutManager linearLayoutManager = (LinearLayoutManager)layoutManager;
+                    if (layoutManager instanceof LinearLayoutManager) {
+                        LinearLayoutManager linearLayoutManager = (LinearLayoutManager) layoutManager;
                         return linearLayoutManager.findFirstCompletelyVisibleItemPosition() == 0;
-                    }else if(layoutManager instanceof StaggeredGridLayoutManager){
-                        StaggeredGridLayoutManager staggeredGridLayoutManager = (StaggeredGridLayoutManager)layoutManager;
+                    } else if (layoutManager instanceof StaggeredGridLayoutManager) {
+                        StaggeredGridLayoutManager staggeredGridLayoutManager = (StaggeredGridLayoutManager) layoutManager;
                         int[] checks = staggeredGridLayoutManager.findFirstCompletelyVisibleItemPositions(null);
-                        for(int check : checks){
-                            if(check == 0)
+                        for (int check : checks) {
+                            if (check == 0)
                                 return true;
                         }
                     }
@@ -324,5 +293,9 @@ public class BounceTouchListener implements View.OnTouchListener {
 
     public void setMaxAbsTranslation(int maxAbsTranslation) {
         this.mMaxAbsTranslation = maxAbsTranslation;
+    }
+
+    public interface OnTranslateListener {
+        void onTranslate(float translation);
     }
 }
